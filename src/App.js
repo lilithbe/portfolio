114357ade918ce41bda1d416fed91bde54d78c04
LILITH-SERVER\server <lilithbe.dev@gmail.com>
2022-04-02 10:23:56 +0900

    s

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
- 
-    
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Portfolio
-        </a>
-      </header>
+    <div >
+   Portfolio
     </div>
   );
 }
